dd6afbc2e303b1d78d800c272a1899b054cac914
luna215 <paulluna0215@gmail.com>
2020-01-06 19:46:52 -0800

    added some of my personal info

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,12 +54,8 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Hi! <strong>{author}</strong> is a writer, philanthropist, and software engineer
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
       </p>
     </div>
   )
