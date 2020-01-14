70421ab818ddf27b0c7458ac267c6b88a0d356cc
luna215 <paulluna0215@gmail.com>
2020-01-13 19:29:39 -0800

    header is now full-width

--- src/components/layout.js
+++ src/components/layout.js
@@ -49,8 +49,8 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          // maxWidth: rhythm(24),
+          // padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
         <header style={{
